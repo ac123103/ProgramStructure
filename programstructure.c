c13fc21ffd6a1aad29714b5ef12b3bb4eb394094
Andrea Ford <ac123103@ohio.edu>
2018-10-04 18:58:34 -0400

    Initial commit

---
+++ programstructure.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+#define MIN 55
+
+// Prototypes:
+int addthree(int);
+void showMessage();
+void checkRange(int);
+
+int main()
+{
+
+    checkRange(105);
+
+    return 0;
+}
+
+int addthree(int x)
+{
+    return x + 3;
+}
+
+void showMessage()
+{
+    printf("Value out of range.\n");
+
+}
+ void checkRange(int enteredValue)
+{
+
+
+
+ if (enteredValue > MAX || enteredValue < MIN)
+        {
+            showMessage();
+        }
+
+}
